ce86e8b8a45e0540b7cfccbd94b78e5ae007a7a1
Xamtastic <anthony.harrison@xamtastic.com>
2016-02-04 10:03:49 +0000

    Initial Copmmit

---
+++ POC/POC/Converters/MaskConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace POC.Converters
+{
+    public class MaskConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool response = false;
+
+            double reading = System.Convert.ToDouble(value);
+            double threshhold = System.Convert.ToDouble(parameter);
+
+            if (reading >= threshhold) response = true;
+
+            return response;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
